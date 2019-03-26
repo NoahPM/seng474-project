b4a9ff39d3f76e14e3d05ff46d4b4efb675b9905
Noah <noahpricemcg@gmail.com>
2019-03-26 15:29:50 -0700

    added json

--- project.py
+++ project.py
@@ -1,33 +1,49 @@
-﻿import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import os
 from PIL import Image
-
+import numpy as np
+import json
 from skimage.feature import hog
 from skimage import data, exposure
-path = "C:\\Users\\noahp\\Documents\\reddit-memes-dataset\\memes"
+
 
 
 def main():
-
+	path = "C:\\Users\\noahp\\Documents\\reddit-memes-dataset"
+	hogs = []
+	fnames = []
+	db = json.loads(open(path + "\\db.json", "r").read())
+	print(json.dumps(db))
+	path += "\\memes"
 	for r, d, f in os.walk(path):
 		for name in f:
 			print(os.path.join(r, name))
 			img = Image.open(os.path.join(r, name))
-			fd, hogi= hog(img, multichannel=True, cells_per_block=(1, 1),  visualize = True, block_norm = "L2-Hys")
-			fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
+			fd= hog(img, multichannel=True, block_norm = "L2-Hys")
+			hogs.append(fd)
+			fnames.append(name)
 
-			ax1.axis('off')
-			ax1.imshow(img, cmap=plt.cm.gray)
-			ax1.set_title('Input image')
+			#visualization (remove later)
+			# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
 
-			# Rescale histogram for better display
-			hogi_rescaled = exposure.rescale_intensity(hogi, in_range=(0, 10))
+			# ax1.axis('off')
+			# ax1.imshow(img, cmap=plt.cm.gray)
+			# ax1.set_title('Input image')
 
-			ax2.axis('off')
-			ax2.imshow(hogi_rescaled, cmap=plt.cm.gray)
-			ax2.set_title('Histogram of Oriented Gradients')
-			plt.show()
-			input()
+			# # Rescale histogram for better display
+			# hogi_rescaled = exposure.rescale_intensity(hogi, in_range=(0, 10))
+
+			# ax2.axis('off')
+			# ax2.imshow(hogi_rescaled, cmap=plt.cm.gray)
+			# ax2.set_title('Histogram of Oriented Gradients')
+			# plt.show()
+			# input()
+			#end visualization
+
+
+
+
+
 
 
 
